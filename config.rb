664125a4e5dad1fff13101902e2973c5783e8cf0
Brandon <bigb@digitalrogues.com>
2013-04-01 17:08:45 -0700

    new post

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/WSRMobile/"
+http_images_path = "/WSRMobile/images"
+http_fonts_path = "/WSRMobile/fonts"
+css_dir = "public/WSRMobile/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
